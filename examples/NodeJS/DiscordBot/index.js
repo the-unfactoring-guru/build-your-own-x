57a021f9b29952158f196681b323046db35b2546
The Unfactoring Guru <theunfactoringguru@gmail.com>
2022-04-23 08:47:30 -0500

    built first file

---
+++ examples/NodeJS/DiscordBot/index.js
@@ -0,0 +1,12 @@
+// Important classes for the program
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready');
+});
+
+
+client.login(token);
